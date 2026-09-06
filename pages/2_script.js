2b6c1bcb69766c3151ca9b2c66dd8e4efbfdacc4
betty <javascript115@gmail.com>
2026-09-06 15:56:40 +0800

    3html

--- pages/2_script.js
+++ pages/2_script.js
@@ -1,12 +1,15 @@
 
-        //註解
-        console.log("123" + 666);
-        /*多行
-        註解*/
-        const text = "Hello, World!";
-        const num = 50 + 666;
-        console.log(text + " " + num);
+//註解
+/*多行
+註解*/
+const text = "Hello, World!";
+const num = 50 + 666;
+console.log(text + " " + num);
 
-        console.error("This is an error message.");
-        console.warn("This is a warning message.");
-        console.info("This is an informational message.");
+/*script要寫在哪都可以，但要注意如果做DOM的話
+*script要放在body內的最後面
+*就是要等到DOM讀取完後才能操作DOM
+*才不會報錯
+*/
+//注意順序，要先有h1才能改變h1的內容
+document.querySelector("h1").innerText = "我把H1文字改掉了";
