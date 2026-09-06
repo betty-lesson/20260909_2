83ec1f138b8afdac758d734868fc5888ed7b537d
betty <javascript115@gmail.com>
2026-09-06 15:31:38 +0800

    2html_add

---
+++ pages/2_script.js
@@ -0,0 +1,12 @@
+
+        //註解
+        console.log("123" + 666);
+        /*多行
+        註解*/
+        const text = "Hello, World!";
+        const num = 50 + 666;
+        console.log(text + " " + num);
+
+        console.error("This is an error message.");
+        console.warn("This is a warning message.");
+        console.info("This is an informational message.");
